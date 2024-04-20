9c81da8874a3191030799b8afabf6122fce9c55a
Maksimich228 <147976922+Maksimich228@users.noreply.github.com>
2024-04-20 20:46:50 +0300

    Add files via upload

---
+++ Graph.h
@@ -0,0 +1,24 @@
+#pragma once
+class Graph
+{
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <vector>
+#include <unordered_set>
+#include "Person.h"
+
+    class Graph {
+    private:
+        std::vector<Person*> people;
+
+    public:
+        void addPerson(Person* person);
+        void addFriendship(Person* person1, Person* person2);
+        void findFriendsOfFriendsWithinDepth(Person* person, int depth, std::unordered_set<Person*>& friendsSet);
+        void findPairsWithinDepth(int depth);
+    };
+
+#endif // GRAPH_H
+};
+
---
+++ Person.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+class Person
+{
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <string>
+#include <vector>
+
+    class Person {
+    public:
+        std::string name;
+        std::vector<Person*> friends;
+
+        Person(std::string n);
+
+        void addFriend(Person* friendPtr);
+    };
+
+#endif // PERSON_H
+};
+
